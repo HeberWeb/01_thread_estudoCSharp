f2949f248c0029649c56d3a6430130bcb208bc8a
Heber Pietrafeza <hpietrafeza@autodoc.com.br>
2019-04-09 17:54:51 -0300

    varias alternativas de threads

---
+++ 05_ThreadSafe/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _05_ThreadSafe
+{
+    class Program
+    {
+        static int Rede = 0;
+        static object variaveDeControle = 0;
+        //IO Input/Output - Lento (Tela, Rede, Armazenamento, Impressora-Rede)
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DataIni: " + DateTime.Now);
+            for (int i = 0; i < 5; i++)
+            {
+                Thread t1 = new Thread(ThreadRepeticao);
+                t1.IsBackground = true; //Ex: Interação das threads via utilização do usuário
+                t1.Start();
+            }
+
+            Console.ReadKey();
+        }
+
+        static void ThreadRepeticao()
+        {
+            //Fila FIFO - First in e First Out
+            for (int i = 0; i < 1000; i++)
+            {
+                lock (variaveDeControle)// Aguarda oque está dentro do lock ser executado para continuar o loop
+                {
+                    Console.WriteLine("Num " + i);
+                    Rede++;
+                }
+            }
+            
+            Console.WriteLine("DataTime: " + DateTime.Now);
+        }
+    }
+}
---
+++ 07_ThreadMethods/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _07_ThreadMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread Sleep
+            Console.WriteLine("Inicio do processo");
+            Thread.Sleep(3000);
+            Console.WriteLine("Fim do processo");
+
+
+            //Thread Join
+            Thread t1 = new Thread(ThreadRepeticao);
+            t1.Start();
+            t1.Join();
+
+            Console.WriteLine("Fim do processo");
+            Console.WriteLine("Fim do processo");
+            Console.WriteLine("Fim do processo");
+            Console.WriteLine("Fim do processo");
+            Console.ReadKey();
+        }
+
+        static void ThreadRepeticao(object id)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Thread: " + id + " Num " + i + " Id Interno: " + Thread.CurrentThread.ManagedThreadId);
+            }
+        }
+    }
+}
